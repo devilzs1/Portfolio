51ea82fc816ac533ebad65b3cbb61a62b39a684f
Mohammad Adil <mohammadadil3292@gmail.com>
2024-03-30 23:09:52 +0530

    update projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,10 +2,20 @@ import Work1 from "../../assets/work1.gif";
 import Work2 from "../../assets/work2.gif";
 import Work3 from "../../assets/work3.gif";
 import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.gif";
 import Work6 from "../../assets/work6.jpg";
-
+import Work8 from "../../assets/work8.gif";
+import Work9 from "../../assets/work9.gif";
 
 export const projectsData = [
+  {
+    id: 0,
+    image: Work8,
+    title: "Baatein chat app",
+    category: "web",
+    // link: "https://baatein-app.vercel.app/",
+    link: "#",
+  },
   {
     id: 1,
     image: Work1,
@@ -22,29 +32,29 @@ export const projectsData = [
   },
   {
     id: 3,
+    image: Work5,
+    title: "NextGen-Compiler",
+    category: "web",
+    link: "https://next-gen-compiler.vercel.app/",
+  },
+  {
+    id: 4,
     image: Work3,
     title: "Monstat",
     category: "web",
     link: "https://devilzs1.github.io/Monstat-Admin-Panel/",
   },
   {
-    id: 4,
+    id: 5,
     image: Work4,
     title: "Web design",
     category: "web",
     link: "#",
   },
   {
-    id: 5,
+    id: 6,
     image: Work4,
     title: "Web design",
-    category: "design",
-    link: "#",
-  },
-  {
-    id: 6,
-    image: Work6,
-    title: "App design",
     category: "design",
     link: "#",
   },
@@ -52,6 +62,27 @@ export const projectsData = [
     id: 7,
     image: Work6,
     title: "App design",
+    category: "design",
+    link: "#",
+  },
+  {
+    id: 8,
+    image: Work6,
+    title: "App design",
+    category: "app",
+    link: "#",
+  },
+  {
+    id: 9,
+    image: Work9,
+    title: "Flutter App design",
+    category: "design",
+    link: "#",
+  },
+  {
+    id: 10,
+    image: Work9,
+    title: "Flutter App design",
     category: "app",
     link: "#",
   },
